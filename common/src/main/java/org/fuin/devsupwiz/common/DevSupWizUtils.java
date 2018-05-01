a7200ff59fe11e71e6698e2f9dbcfd6e641e4a8a
Michael Schnell <michael@fuin.org>
2018-05-01 15:47:34 +0200

    Moved Jandex helper methods to Utils4J

--- common/src/main/java/org/fuin/devsupwiz/common/DevSupWizUtils.java
+++ common/src/main/java/org/fuin/devsupwiz/common/DevSupWizUtils.java
@@ -20,27 +20,23 @@ package org.fuin.devsupwiz.common;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.attribute.PosixFilePermission;
 import java.text.MessageFormat;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.Set;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Path;
 
-import org.fuin.utils4j.Utils4J;
+import org.fuin.utils4j.JandexUtils;
 import org.jboss.jandex.ClassInfo;
 import org.jboss.jandex.DotName;
 import org.jboss.jandex.Index;
@@ -61,8 +57,7 @@ public final class DevSupWizUtils {
     /** Key used for the MDC 'task' value. */
     public static final String MDC_TASK_KEY = "task";
 
-    private static final Logger LOG = LoggerFactory
-            .getLogger(DevSupWizUtils.class);
+    private static final Logger LOG = LoggerFactory.getLogger(DevSupWizUtils.class);
 
     private DevSupWizUtils() {
     }
@@ -81,11 +76,9 @@ public final class DevSupWizUtils {
      * @param <T>
      *            Type of the setup task.
      */
-    public static <T extends SetupTask> boolean violated(
-            final Set<ConstraintViolation<T>> violations, final String name) {
+    public static <T extends SetupTask> boolean violated(final Set<ConstraintViolation<T>> violations, final String name) {
         for (ConstraintViolation<T> violation : violations) {
-            final Path.Node node = violation.getPropertyPath().iterator()
-                    .next();
+            final Path.Node node = violation.getPropertyPath().iterator().next();
             if (node.getName().equals(name)) {
                 return true;
             }
@@ -106,8 +99,7 @@ public final class DevSupWizUtils {
             out.write(message.getBytes());
             out.write(System.lineSeparator().getBytes());
         } catch (final IOException ex) {
-            throw new RuntimeException(
-                    "Wasn't able to write message to output: " + message, ex);
+            throw new RuntimeException("Wasn't able to write message to output: " + message, ex);
         }
     }
 
@@ -124,13 +116,10 @@ public final class DevSupWizUtils {
      *            Expected appender type.
      */
     @SuppressWarnings("unchecked")
-    public static <T extends Appender<ILoggingEvent>> T findAppender(
-            final String name) {
-        final LoggerContext context = (LoggerContext) LoggerFactory
-                .getILoggerFactory();
+    public static <T extends Appender<ILoggingEvent>> T findAppender(final String name) {
+        final LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
         for (ch.qos.logback.classic.Logger logger : context.getLoggerList()) {
-            for (Iterator<Appender<ILoggingEvent>> index = logger
-                    .iteratorForAppenders(); index.hasNext();) {
+            for (Iterator<Appender<ILoggingEvent>> index = logger.iteratorForAppenders(); index.hasNext();) {
                 final Appender<ILoggingEvent> appender = index.next();
                 if (name.equals(appender.getName())) {
                     return (T) appender;
@@ -152,8 +141,7 @@ public final class DevSupWizUtils {
      * 
      * @return Message.
      */
-    public static String getString(final ResourceBundle bundle,
-            final String key, final Object... params) {
+    public static String getString(final ResourceBundle bundle, final String key, final Object... params) {
         return MessageFormat.format(bundle.getString(key), params);
     }
 
@@ -164,16 +152,14 @@ public final class DevSupWizUtils {
      */
     public static String getHostname() {
         final ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        final ShellCommandExecutor executor = new ShellCommandExecutor(
-                "hostname", 5, new HashMap<String, String>(), bos,
+        final ShellCommandExecutor executor = new ShellCommandExecutor("hostname", 5, new HashMap<String, String>(), bos,
                 new LogOutputStream(Level.ERROR));
         final int result = executor.execute();
         final String text = new String(bos.toByteArray());
         if (result == 0) {
             return text.trim();
         } else {
-            throw new RuntimeException(
-                    "Error # " + result + " reading hostname: " + text);
+            throw new RuntimeException("Error # " + result + " reading hostname: " + text);
         }
     }
 
@@ -185,8 +171,7 @@ public final class DevSupWizUtils {
      * @param permissions
      *            Permissions to set.
      */
-    public static void setFilePermissions(final File file,
-            final PosixFilePermission... permissions) {
+    public static void setFilePermissions(final File file, final PosixFilePermission... permissions) {
         try {
             final Set<PosixFilePermission> perms = new HashSet<PosixFilePermission>();
             for (final PosixFilePermission permission : permissions) {
@@ -198,68 +183,14 @@ public final class DevSupWizUtils {
         }
     }
 
-    private static List<String> implementors(final Index index,
-            final Class<?> clasz) {
+    private static List<String> implementors(final Index index, final Class<?> clasz) {
         final List<String> classNames = new ArrayList<>();
-        final Set<ClassInfo> implementors = index
-                .getAllKnownImplementors(DotName.createSimple(clasz.getName()));
+        final Set<ClassInfo> implementors = index.getAllKnownImplementors(DotName.createSimple(clasz.getName()));
         for (final ClassInfo ci : implementors) {
             classNames.add(ci.name().toString());
         }
         return classNames;
-    }
-
-    private static void indexClassFile(final Indexer indexer,
-            final List<File> knownFiles, final File classFile) {
-        if (!knownFiles.contains(classFile)) {
-            knownFiles.add(classFile);
-            LOG.trace("Analyze class file: " + classFile);
-            try (final InputStream in = classFile.toURI().toURL()
-                    .openStream()) {
-                indexer.index(in);
-            } catch (final IOException ex) {
-                throw new RuntimeException("Error indexing file: " + classFile,
-                        ex);
-            }
-        }
-    }
-
-    private static void indexDir(final Indexer indexer,
-            final List<File> knownFiles, final File dir) {
-        LOG.debug("Index dir: " + dir);
-        final List<File> classes = Utils4J.pathsFiles(dir.getPath(),
-                Utils4J::classFile);
-        for (final File file : classes) {
-            indexClassFile(indexer, knownFiles, file);
-        }
-    }
-
-    private static void indexJar(final Indexer indexer,
-            final List<File> knownFiles, final File jarFile) {
-        LOG.debug("Index jar: " + jarFile);
-        if (!knownFiles.contains(jarFile)) {
-            knownFiles.add(jarFile);
-        }
-
-        try (final JarFile jar = new JarFile(jarFile)) {
-            final Enumeration<JarEntry> entries = jar.entries();
-            while (entries.hasMoreElements()) {
-                final JarEntry entry = entries.nextElement();
-                if (entry.getName().endsWith(".class")) {
-                    LOG.trace("Analyze class: " + entry.getName());
-                    try (final InputStream stream = jar.getInputStream(entry)) {
-                        indexer.index(stream);
-                    } catch (final IOException ex) {
-                        throw new RuntimeException("Error indexing "
-                                + entry.getName() + " in " + jarFile, ex);
-                    }
-                }
-
-            }
-        } catch (final IOException ex) {
-            throw new RuntimeException("Error indexing " + jarFile, ex);
-        }
-    }
+    }   
 
     /**
      * Returns all classes from the classpath (*.jar or *.class) that implement
@@ -268,33 +199,9 @@ public final class DevSupWizUtils {
      * @return List of full qualified class names.
      */
     public static List<String> findSetupTasksInClasspath() {
-
+        final List<File> knownFiles = new ArrayList<File>();
         final Indexer indexer = new Indexer();
-
-        // Prevent jars/classes to be analyzed twice
-        final List<File> knownFiles = new ArrayList<>();
-
-        // Variant that works with Maven "exec:java"
-        final List<File> classPathFiles = Utils4J.localFilesFromUrlClassLoader(
-                (URLClassLoader) DevSupWizUtils.class.getClassLoader());
-        for (final File file : classPathFiles) {
-            LOG.debug("CP file: {}", file);
-            if (Utils4J.nonJreJarFile(file)) {
-                indexJar(indexer, knownFiles, file);
-            } else if (file.isDirectory() && !file.getName().startsWith(".")) {
-                indexDir(indexer, knownFiles, file);
-            }
-        }
-
-        // Variant that works for Maven surefire tests
-        for (final File file : Utils4J.classpathFiles(Utils4J::nonJreJarFile)) {
-            indexJar(indexer, knownFiles, file);
-        }
-        for (final File file : Utils4J.classpathFiles(Utils4J::classFile)) {
-            indexClassFile(indexer, knownFiles, file);
-        }
-
-        // Finalize index and return implementors
+        JandexUtils.indexClasspath((URLClassLoader) DevSupWizFxUtils.class.getClassLoader(), indexer, knownFiles);
         final Index index = indexer.complete();
         return implementors(index, SetupTask.class);
 
